b761fc5afcc28b15ef458cb0a40fc3e2144f9369
showkathkl <2500030098@kluniversity.in>
2026-03-27 11:35:14 +0530

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  base:"/Studatrix-zip-1-/",
   plugins: [react()],
+  base: '/Studatrix-zip-1-/'
 })
